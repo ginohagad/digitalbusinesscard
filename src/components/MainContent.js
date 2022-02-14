00fae2ddcf5a14e4efd3a7433cf65a96f01ac8be
Gino <ginohagad@gmail.com>
2022-02-14 21:59:29 +1300

    Trying to add FontAwesome icon to email and LinkedIn buttons.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,7 +4,11 @@ export default function MainContent() {
     <main>
       <h1 className="main--name">Gino Hagad</h1>
       <h4 className="main--position">Frontend Developer</h4>
-      <span className="main--website">ginohagad.com</span>
+      <p className="main--website">ginohagad.com</p>
+      <div className="main--buttons">
+        <button className="button-email"><FontAwesomeIcon icon="fa-solid fa-envelope" />Email</button>
+        <button className="button-linkedin">LinkedIn</button>
+      </div>
       <h3 className="main--section-title">About</h3>
       <p className="main--content">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and I am always looking for new things to learn.</p>
       <h3 className="main--section-title">Interests</h3>
