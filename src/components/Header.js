38423b33a125074fb22882f5579d558c9ccc9f8a
Gino <ginohagad@gmail.com>
2022-02-14 21:35:42 +1300

    First commit.

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import profilepic from '../images/profilepic.jpeg'
+
+export default function Header() {
+
+  return (
+    <div className="header--image">
+      <img src={profilepic} />
+    </div>
+  )
+}
+
---
+++ src/components/MainContent.js
@@ -0,0 +1,14 @@
+export default function MainContent() {
+
+  return (
+    <main>
+      <h1 className="main--name">Gino Hagad</h1>
+      <h4 className="main--position">Frontend Developer</h4>
+      <span className="main--website">ginohagad.com</span>
+      <h3 className="main--section-title">About</h3>
+      <p className="main--content">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and I am always looking for new things to learn.</p>
+      <h3 className="main--section-title">Interests</h3>
+      <p className="main--content">Food expert. Music Production. Internet fanatic. Coffee fanatic.</p>
+    </main>
+  )
+}
